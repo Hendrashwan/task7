4fc7f48a5fb460cbfca7b4902b5e24d4f6bda603
Hendrashwan <hend_farag2012@yahoo.com>
2024-06-09 17:34:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name = prompt("mobile");
+let product = prompt("samsung");
+let price = parseFloat(prompt("10000"));
+
+// Calculate the profit margin
+let profitMargin = 10000 * 1.1;
+
+// Calculate the selling price after adding taxes
+let sellingPrice = profitMargin * 1.05;
+// Target the HTML element with the ID "result"
+let resultElement = document.getElementById("result");
